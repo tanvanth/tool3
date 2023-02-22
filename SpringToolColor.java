c20b0bd4c56575516c6265ab381e19680bad4b38
tanvanth <113883910+tanvanth@users.noreply.github.com>
2023-02-22 19:09:21 +0530

    Add files via upload

---
+++ SpringToolColor.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class task3 {
+	@RequestMapping(value="/MyFav/{color}",method=RequestMethod.GET)
+	public String getMyFav(@PathVariable String color)
+	{
+		return "My favorite color is "+color;
+	}
+}
